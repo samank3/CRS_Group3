d8c998d4499a18abe7937a6dbdb5eee8294bbdf2
rpsharma98 <rpsharma.my.yorku.ca>
2018-01-24 13:00:31 -0500

    init commit for rpsa

--- Enamel/src/authoring/Authoring.java
+++ Enamel/src/authoring/Authoring.java
@@ -31,7 +31,7 @@ public class Authoring {
 		chooseScenario.getAccessibleContext().setAccessibleDescription("Choose a damn Scenario.");
 		
 		chooseScenario.addActionListener(new ActionListener() {
-
+			
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
