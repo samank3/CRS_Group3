3021535b4720c7dd517fcd421af7f7284606d58d
rpsharma98 <rpsharma.my.yorku.ca>
2018-02-07 22:37:32 -0500

    Updated Gui, Added a JMenuBar as well as file validation.

--- Enamel/src/authoring/Authoring.java
+++ Enamel/src/authoring/Authoring.java
@@ -9,73 +9,165 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.UIManager;
+import java.awt.ComponentOrientation;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
 
 public class Authoring {
 	//Branch Upper
 	private static JFrame frame;
+	private static JButton chooseScenario;
+	private static JFileChooser file_chooser;
+	private static JTextArea scenarioReader;
+	
+	private static void openFileChooser() {
+		if(file_chooser.showOpenDialog(chooseScenario) == JFileChooser.APPROVE_OPTION) {
+			
+			StringBuffer buff = new StringBuffer();
+			scenarioReader.setText("");
+			int lineCounter = 0;
+			try {
+				Scanner scanner = new Scanner(file_chooser.getSelectedFile());
+				while(scanner.hasNextLine()) {
+					//Check if first two lines follow the specific plan.
+					String line = scanner.nextLine();
+					if(lineCounter == 0) {
+						if(!line.startsWith("Cell")) {
+							System.out.println("File: " + file_chooser.getSelectedFile() + " is not a valid format");
+							frame.setTitle("Authoring Application - Invalid Format");
+							scanner.close();
+							return;
+						}
+					}
+					if(lineCounter == 1) {
+						if(!line.startsWith("Button")) {
+							System.out.println("File: " + file_chooser.getSelectedFile() + " is not a valid format");
+							frame.setTitle("Authoring Application - Invalid Format");
+							scanner.close();
+							return;
+						}
+					}
+					lineCounter+=1;
+					buff.append(line+"\n");
+				}
+				scanner.close();
+				//System.out.println(buff.toString());
+				scenarioReader.setText(buff.toString());
+				System.out.println("File: " + file_chooser.getSelectedFile() + " has been imported");
+				frame.setTitle("Authoring Application - " + file_chooser.getSelectedFile());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+				
+			
+		}else {
+			System.out.println("Error Selecting File");
+			frame.setTitle("Authoring Application - Error Selecting File");
+		}
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		frame = new JFrame();
-		frame.setBounds(100,100,450,300);
+		frame.setBounds(100,100,648,428);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 		//frame.setVisible(true);
 		frame.setTitle("Authoring Application");
 
-		JFileChooser file_chooser = new JFileChooser();
+		file_chooser = new JFileChooser();
 		file_chooser.setCurrentDirectory(new java.io.File("C:"));
 		file_chooser.setDialogTitle("Open Scenario File");
-		JButton chooseScenario = new JButton("Choose Scenario");
+		chooseScenario = new JButton("Choose Scenario");
 		chooseScenario.setVisible(true);
 		
-		JTextArea Tarea = new JTextArea();
+		scenarioReader = new JTextArea();
+		scenarioReader.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		scenarioReader.setBorder(UIManager.getBorder("FileChooser.listViewBorder"));
+		scenarioReader.setWrapStyleWord(true);
+		scenarioReader.setLineWrap(true);
+		scenarioReader.setEditable(false);
 
 		chooseScenario.getAccessibleContext().setAccessibleName("Choose a Scenario");
 		chooseScenario.getAccessibleContext().setAccessibleDescription("Choose a damn Scenario.");
 		
+		JScrollPane scroll = new JScrollPane(scenarioReader);
+
+		
 		chooseScenario.addActionListener(new ActionListener() {
 
+			
+			
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0){
 				// TODO Auto-generated method stub
-				if(file_chooser.showOpenDialog(chooseScenario) == JFileChooser.APPROVE_OPTION) {
-					System.out.println("File: " + file_chooser.getSelectedFile() + " has been imported");
-					frame.setTitle("Authoring Application - " + file_chooser.getSelectedFile());
-					StringBuffer buff = new StringBuffer();
-					Tarea.setText("");
-					try {
-						Scanner scanner = new Scanner(file_chooser.getSelectedFile());
-						while(scanner.hasNext()) {
-							buff.append(scanner.nextLine()+"\n");
-						}
-						scanner.close();
-						//System.out.println(buff.toString());
-						Tarea.setText(buff.toString());
-					} catch (FileNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-						
-					
-				}else {
-					System.out.println("Error Selecting File");
-					frame.setTitle("Authoring Application - Error Selecting File");
-				}
+				openFileChooser();
+			}
+			
+		});
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(chooseScenario, GroupLayout.PREFERRED_SIZE, 152, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scenarioReader))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(3)
+					.addComponent(scenarioReader, GroupLayout.DEFAULT_SIZE, 386, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(chooseScenario, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(359, Short.MAX_VALUE))
+		);
+		frame.getContentPane().setLayout(groupLayout);
+		scenarioReader.setVisible(true);
+		frame.getContentPane().add(scroll);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("File");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mnNewMenu.add(mntmOpen);
+		
+		mntmOpen.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				openFileChooser();
 			}
 			
 		});
 		
-		chooseScenario.setBounds(450,300,200,43);
-		Tarea.setBounds(0,0,450,300);
-		frame.getContentPane().add(chooseScenario);
-		frame.getContentPane().add(Tarea);
-		Tarea.setVisible(true);
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mnNewMenu.add(mntmSave);
+		
+		mntmSave.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				//System.out.println("Save Pls");
+				
+			}
+			});
+		
 		frame.setVisible(true);
 		
 		
 	}
-
 }
